37d4a5a4adf14e050fe6d7b1f44ad79fe75cb51a
ptamas <ptamas@ilona-PowerEdge-T20>
2017-07-03 14:50:05 +0200

    - change separator name to file_separator

--- ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/fileOperations/DestinationDirectory.java
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/fileOperations/DestinationDirectory.java
@@ -5,20 +5,20 @@ import java.io.IOException;
 
 public class DestinationDirectory {
 
-	private static final String SEPARATOR = "file.separator";
+	private static final String FILE_SEPARATOR = "file.separator";
 	
 	public static File createDestinationDir(File file, String destinationDirectoryName) throws IOException {
-		File directory = new File(file.getParentFile().getAbsolutePath() + System.getProperty(SEPARATOR) + destinationDirectoryName);
+		File directory = new File(file.getParentFile().getAbsolutePath() + System.getProperty(FILE_SEPARATOR) + destinationDirectoryName);
 		if(directory.exists() == false) {
 			if(directory.mkdir() == false) {
 				throw new IOException("Could not create directory: " + directory.getAbsolutePath());
 			}
 		}
 		int versionNo = 0;
-		while(new File(directory.getAbsolutePath() + System.getProperty(SEPARATOR) + "version_" + versionNo).exists() == true) {
+		while(new File(directory.getAbsolutePath() + System.getProperty(FILE_SEPARATOR) + "version_" + versionNo).exists() == true) {
 			versionNo++;
 		}
-		File destinationDirectory = new File(directory.getAbsolutePath() + System.getProperty(SEPARATOR) + versionNo);
+		File destinationDirectory = new File(directory.getAbsolutePath() + System.getProperty(FILE_SEPARATOR) + versionNo);
 		if(destinationDirectory.mkdir() == false) {
 			throw new IOException("Could not create directory: " + destinationDirectory.getAbsolutePath());
 		}
