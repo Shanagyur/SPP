a35c40810b8b8d0c2decbedb790ccd5e095bad44
ptamas <ptamas@ilona-PowerEdge-T20>
2017-07-03 13:22:32 +0200

    - complete getWantedFile method

--- ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/fileOperations/WantedFile.java
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/fileOperations/WantedFile.java
@@ -2,42 +2,38 @@ package hu.uni.miskolc.iit.spp.latex.fileOperations;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.HashSet;
 
 import hu.uni.miskolc.iit.spp.core.model.exception.WantedFileNotExistsException;
+import hu.uni.miskolc.iit.spp.latex.investigations.ExtensionTest;
+import hu.uni.miskolc.iit.spp.latex.investigations.FileNameTest;
 
 public class WantedFile {
 
 	private File directory;
 	private Collection<String> possibleFileNames;
+	private Collection<String> possibleFileExtensions;
 	
-	private WantedFile(File directory, Collection<String> possibleFileNames) {
+	public WantedFile(File directory, Collection<String> possibleFileNames, Collection<String> possibleFileExtensions) {
 		this.directory = directory;
 		this.possibleFileNames = possibleFileNames;
+		this.possibleFileExtensions = possibleFileExtensions;
 	}
-	
-	public static WantedFile createWantedFileClassOnePossibleFileName(File directory, String possibleFileName) {
-		Collection<String> possibleFileNames = new HashSet<>();
-		possibleFileNames.add(possibleFileName);
-		return new WantedFile(directory, possibleFileNames);
-	}
-	
-	public static WantedFile createWantedFileClassMorePossibleFileNames(File directory, Collection<String> possibleFileNames) {
-		return new WantedFile(directory, possibleFileNames);
-	}
-	
+
 	public File getWantedFile() throws WantedFileNotExistsException {
 		File[] files = this.directory.listFiles();
 		for(File file : files) {
 			if(file.isDirectory() == true) {
-				WantedFile dir = new WantedFile(file, this.possibleFileNames);
+				WantedFile dir = new WantedFile(file, this.possibleFileNames, this.possibleFileExtensions);
 				dir.getWantedFile();
 			} else {
-				if(this.possibleFileNames.contains(file.getName()) == true) {
+				FileNameTest testedFileName = new FileNameTest(file, this.possibleFileNames);
+				ExtensionTest testedExtension = new ExtensionTest(file, this.possibleFileExtensions);
+				if((testedFileName.eitherMatch() && testedExtension.extensionTest()) == true) {
 					return file;
 				}
 			}
 		}
-		throw new WantedFileNotExistsException();
+		throw new WantedFileNotExistsException("Could not find file with that name(s): " + this.possibleFileNames.toString() + 
+												"and with that extension(s): " + this.possibleFileExtensions.toString());
 	}
 }
