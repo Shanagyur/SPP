5cf5be32e5818f57d92f0dc1bf397df67df975a4
ptamas <ptamas@ilona-PowerEdge-T20>
2017-07-03 13:18:58 +0200

    - remove some methods

--- ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/investigations/ExtensionTest.java
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/investigations/ExtensionTest.java
@@ -8,27 +8,15 @@ import org.apache.commons.io.FilenameUtils;
 public class ExtensionTest {
 
 	private File file;
-	private Collection<String> possibleExtension;
+	private Collection<String> possibleExtensions;
 
-	private ExtensionTest(File file, Collection<String> possibleExtension) {
+	public ExtensionTest(File file, Collection<String> possibleExtension) {
 		this.file = file;
-		this.possibleExtension = possibleExtension;
+		this.possibleExtensions = possibleExtension;
 	}
 
-	public static ExtensionTest createTexTestClass(File file, Collection<String> possibleTexFileExtension) {
-		return new ExtensionTest(file, possibleTexFileExtension);
-	}
-	
-	public static ExtensionTest createArchiveTestClass(File file, Collection<String> possibleArchiveFileExtension) {
-		return new ExtensionTest(file, possibleArchiveFileExtension);
-	}
-	
-	public static ExtensionTest createGeneratedFileTestClass(File file, Collection<String> possibleGeneratedFileExtension) {
-		return new ExtensionTest(file, possibleGeneratedFileExtension);
-	}
-	
 	public boolean extensionTest() {
 		String fileExtension = FilenameUtils.getExtension(this.file.toString());
-		return this.possibleExtension.contains(fileExtension);
+		return this.possibleExtensions.contains(fileExtension);
 	}
 }
