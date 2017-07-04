6cb1aa6cb882181856b916eedf18052e5fe7e107
ptamas <ptamas@ilona-PowerEdge-T20>
2017-07-04 14:09:44 +0200

    - add constructor, change possibleArchiveExtension type and name, edit unzip method

--- ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/fileOperations/Unpacking.java
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/fileOperations/Unpacking.java
@@ -2,34 +2,31 @@ package hu.uni.miskolc.iit.spp.latex.fileOperations;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashSet;
 
-import hu.uni.miskolc.iit.spp.core.model.exception.NotSupportedFileExtensionException;
+import hu.uni.miskolc.iit.spp.core.model.SupportedArchiveExtensions;
+import hu.uni.miskolc.iit.spp.core.model.exception.NotSupportedArchiveExtensionException;
 import hu.uni.miskolc.iit.spp.latex.investigations.ExtensionTest;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
 public class Unpacking {
 
-	private static Collection<String> possibleArchiveExtension;
+	private static SupportedArchiveExtensions supportedArchiveExtensions;
 	private static final String DESTIONATION_DIR_NAME = "targetDir";
 	
-	static {
-		possibleArchiveExtension = new HashSet<>();
-		possibleArchiveExtension.add("zip");
+	public Unpacking() {
 	}
-	
-	public static File unzip(File zipFile) throws NotSupportedFileExtensionException, IOException {
-		ExtensionTest testedFile = new ExtensionTest(zipFile, possibleArchiveExtension);
-		if(testedFile.extensionTest() == false) {
-			throw new NotSupportedFileExtensionException("Could not extract the file, because not a .zip file: " + zipFile.getAbsolutePath());
+
+	public static File unzip(File zipFile) throws NotSupportedArchiveExtensionException, IOException {
+		ExtensionTest testedFile = new ExtensionTest(zipFile);
+		if(testedFile.archiveExtensionSupported() == false) {
+			throw new NotSupportedArchiveExtensionException("Could not extract " + zipFile.getAbsolutePath() + " because not a zip file.");
 		}
 		try {
 			ZipFile validZipFile = new ZipFile(zipFile);
-			File directoryWithExtractFiles = DestinationDirectory.createDestinationDir(zipFile, DESTIONATION_DIR_NAME);
-			validZipFile.extractAll(directoryWithExtractFiles.getAbsolutePath());
-			return directoryWithExtractFiles; 
+			File directoryForExtractedFiles = DestinationDirectory.createDestinationDir(zipFile, DESTIONATION_DIR_NAME);
+			validZipFile.extractAll(directoryForExtractedFiles.getAbsolutePath());
+			return directoryForExtractedFiles;
 		} catch (ZipException e) {
 			throw new IOException(e.getMessage());
 		} catch (IOException e) {
