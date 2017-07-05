19f37f2ad8649e0448f87314826a7a6854f01e15
ptamas <pinter7@iit.uni-miskolc.hu>
2017-07-05 13:24:48 +0200

    - parent class for WindowsLatexPDFCompiler and LinuxLatexPDFCompiler

---
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/Latex2PDFCompiler.java
@@ -0,0 +1,37 @@
+package hu.uni.miskolc.iit.spp.latex.compile;
+
+import java.io.File;
+import java.io.IOException;
+
+import hu.uni.miskolc.iit.spp.core.model.exception.ConversionToPDFException;
+import hu.uni.miskolc.iit.spp.core.model.exception.SearchedFileNotExistsException;
+import hu.uni.miskolc.iit.spp.latex.fileOperations.FileSearch;
+
+public abstract class Latex2PDFCompiler {
+
+	protected String compilerArg;
+	protected String outputDirArg;
+	
+	protected Latex2PDFCompiler() {
+		init();
+	}
+	
+	public File generatePDFFile(File sourceDir, File destinationDir) throws ConversionToPDFException {
+		try {
+			compile(sourceDir, destinationDir);
+			FileSearch file = new FileSearch(destinationDir);
+			File pdfFile = file.findPDFFile();
+			return pdfFile;
+		} catch (SearchedFileNotExistsException e) {
+			throw new ConversionToPDFException(e.getMessage());
+		} catch (IOException e) {
+			throw new ConversionToPDFException(e.getMessage());
+		}
+	}
+	
+	protected abstract void init();
+	
+	protected abstract void compile(File sourceDir, File destinationDir) throws SearchedFileNotExistsException, IOException;
+	
+	protected abstract String commandForTerminal(File sourceDir, File destinationDir) throws SearchedFileNotExistsException;
+}
