f803de3f4e07d29711a3bb64a7f6bca754d4fa95
ptamas <pinter7@iit.uni-miskolc.hu>
2017-07-05 13:23:02 +0200

    create, abtract class,

---
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/LinuxLatexPDFCompiler.java
@@ -0,0 +1,43 @@
+package hu.uni.miskolc.iit.spp.latex.compile;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import hu.uni.miskolc.iit.spp.core.model.exception.SearchedFileNotExistsException;
+import hu.uni.miskolc.iit.spp.latex.fileOperations.FileSearch;
+
+public class LinuxLatexPDFCompiler extends Latex2PDFCompiler {
+
+	protected LinuxLatexPDFCompiler() {
+		super();
+	}
+
+	@Override
+	protected void init() {
+		this.compilerArg = LinuxArgs.COMPILER.getArgument();
+		this.outputDirArg = LinuxArgs.OUTPUT.getArgument();
+	}
+
+	@Override
+	protected void compile(File sourceDir, File destinationDir) throws SearchedFileNotExistsException, IOException {
+		Process process = Runtime.getRuntime().exec(commandForTerminal(sourceDir, destinationDir));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		
+		String string;
+		while((string = reader.readLine()) != null) {
+		}
+	}
+
+	@Override
+	protected String commandForTerminal(File sourceDir, File destinationDir) throws SearchedFileNotExistsException {
+		FileSearch file = new FileSearch(sourceDir);
+		File mainFile = file.findTexFile();
+		String fullCommand =	this.compilerArg + 
+								this.outputDirArg + 
+								destinationDir.getAbsolutePath() + " " + 
+								mainFile.getAbsolutePath();
+		return fullCommand;
+	}
+}
