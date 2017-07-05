a58c8491ee0c10ead47221585711b80c69c90d56
ptamas <ptamas@ilona-PowerEdge-T20>
2017-07-05 09:31:18 +0200

    - change possibleOperationSystem name and type to supportedOperationSystems and enum, remove getInArray method

--- ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/investigations/OperationSystemTest.java
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/investigations/OperationSystemTest.java
@@ -1,31 +1,23 @@
 package hu.uni.miskolc.iit.spp.latex.investigations;
 
-import java.util.Collection;
-
+import hu.uni.miskolc.iit.spp.core.model.SupportedOperationSystems;
 import hu.uni.miskolc.iit.spp.core.model.exception.NotSupportedOperationSystemException;
 
 public class OperationSystemTest {
 
 	private String osName;
-	private Collection<String> possibleOperationSystem;
+	private static SupportedOperationSystems supportedOperationSystems;
 	
-	public OperationSystemTest(String osName, Collection<String> possibleOperationSystem) {
+	public OperationSystemTest(String osName) {
 		this.osName = osName;
-		this.possibleOperationSystem = possibleOperationSystem;
 	}
 	
 	public String whichMatch() throws NotSupportedOperationSystemException {
-		for(String name : OperationSystemTest.getInArray(this.possibleOperationSystem)) {
-			if(this.osName.toLowerCase().contains(name) == true) {
-				return name;
+		for(SupportedOperationSystems name : OperationSystemTest.supportedOperationSystems.values()) {
+			if(this.osName.toLowerCase().contains(name.getStringValue()) == true) {
+				return name.getStringValue();
 			}
 		}
-		throw new NotSupportedOperationSystemException("Could not compile because the operation system (" + this.osName + ") not suppoerted.\n"
-													+ "Supoorted operation systems: " + this.possibleOperationSystem.toString());
-	}
-	
-	private static String[] getInArray(Collection<String> possibleOperationSystem) {
-		String[] possibleOperationSystemInArray = possibleOperationSystem.toArray(new String[possibleOperationSystem.size()]);
-		return possibleOperationSystemInArray;
+		throw new NotSupportedOperationSystemException("Operation system not supported");
 	}
 }
