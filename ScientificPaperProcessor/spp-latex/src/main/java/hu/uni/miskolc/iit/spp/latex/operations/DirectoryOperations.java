aa0dcff59fd67168f15d3ed17bef3642d2046291
ptamas <pinter7@iit.uni-miskolc.hu>
2017-07-06 14:22:32 +0200

    - rename, add constructor, add new field for "version_"

---
+++ ScientificPaperProcessor/spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/operations/DirectoryOperations.java
@@ -0,0 +1,30 @@
+package hu.uni.miskolc.iit.spp.latex.operations;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DirectoryOperations {
+
+	private static final String FILE_SEPARATOR = "file.separator";
+	
+	public DirectoryOperations() {
+	}
+	
+	public static File createDestinationDir(File file, String destinationDirectoryName) throws IOException {
+		File directory = new File(file.getParentFile().getAbsolutePath() + System.getProperty(FILE_SEPARATOR) + destinationDirectoryName);
+		if(directory.exists() == false) {
+			if(directory.mkdir() == false) {
+				throw new IOException("Could not create directory: " + directory.getAbsolutePath());
+			}
+		}
+		int versionNo = 0;
+		while(new File(directory.getAbsolutePath() + System.getProperty(FILE_SEPARATOR) + "version_" + versionNo).exists() == true) {
+			versionNo++;
+		}
+		File destinationDirectory = new File(directory.getAbsolutePath() + System.getProperty(FILE_SEPARATOR) + versionNo);
+		if(destinationDirectory.mkdir() == false) {
+			throw new IOException("Could not create directory: " + destinationDirectory.getAbsolutePath());
+		}
+		return destinationDirectory;
+	}
+}
