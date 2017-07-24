515b547cac56028eb9003070cfaaf3e809962c2b
ptamas <p.tamas93@gmail.com>
2017-07-24 13:09:39 +0200

    - moved here from latex package

--- spp-core/src/main/java/hu/uni/miskolc/iit/spp/core/service/archive/ArchiveScientificPaperBuilder.java
+++ spp-core/src/main/java/hu/uni/miskolc/iit/spp/core/service/archive/ArchiveScientificPaperBuilder.java
@@ -1,16 +1,22 @@
 package hu.uni.miskolc.iit.spp.core.service.archive;
 
 import hu.uni.miskolc.iit.spp.core.model.ScientificPaper;
+import hu.uni.miskolc.iit.spp.core.model.SupportedArchiveExtensions;
 import hu.uni.miskolc.iit.spp.core.model.exception.ConversionToPDFException;
 import hu.uni.miskolc.iit.spp.core.model.exception.NoMainDocumentFoundException;
 import hu.uni.miskolc.iit.spp.core.service.ScientificPaperBuilder;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 
 public abstract class ArchiveScientificPaperBuilder implements ScientificPaperBuilder {
 
+    private static Logger LOG = LogManager.getLogger(ArchiveScientificPaperBuilder.class);
     private ScientificPaperBuilder builder;
+    private ArchiveScientificPaperBuilder archiveBuilder;
 
     public ArchiveScientificPaperBuilder(ScientificPaperBuilder builder) {
         this.builder = builder;
@@ -22,13 +28,24 @@ public abstract class ArchiveScientificPaperBuilder implements ScientificPaperBu
     }
 
     @Override
-    public ScientificPaper build(File paper) throws NoMainDocumentFoundException, ConversionToPDFException, IOException {
-        checkFileExtension(paper);
+    public ScientificPaper build(File paper) throws NoMainDocumentFoundException, IOException, ConversionToPDFException {
+        if(!isSupportedArchive(paper)) {
+            LOG.fatal("Throw IOException this message:  Not supported compression format.");
+            throw new IOException("Not supported compression format.");
+        }
         File extractedArchiveDir = extract(paper);
         return builder.build(extractedArchiveDir);
     }
 
-    protected abstract  void checkFileExtension(File paper) throws NoMainDocumentFoundException, IOException;
+    protected abstract File extract(File archive) throws IOException;
 
-    protected abstract File extract(File archive);
+    private boolean isSupportedArchive(File file) {
+        String fileExtension = FilenameUtils.getExtension(file.getName());
+        for(SupportedArchiveExtensions extension : SupportedArchiveExtensions.values()) {
+            if(fileExtension.equals(extension.getStringValue())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- spp-core/src/main/java/hu/uni/miskolc/iit/spp/core/service/archive/ZipArchiveSPBuilder.java
+++ spp-core/src/main/java/hu/uni/miskolc/iit/spp/core/service/archive/ZipArchiveSPBuilder.java
@@ -1,26 +1,59 @@
 package hu.uni.miskolc.iit.spp.core.service.archive;
 
-import hu.uni.miskolc.iit.spp.core.model.exception.NoMainDocumentFoundException;
+import hu.uni.miskolc.iit.spp.core.model.UsedDirectoryNames;
 import hu.uni.miskolc.iit.spp.core.service.ScientificPaperBuilder;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 
 public class ZipArchiveSPBuilder extends ArchiveScientificPaperBuilder {
 
+    private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+    private static final String DEST_DIR_NAME = UsedDirectoryNames.DIR_FOR_EXTRACT_FILES.getStringValue();
+    private static final String SUB_DIR_NAME = "version_";
+    private static Logger LOG = LogManager.getLogger(ZipException.class);
+
     public ZipArchiveSPBuilder(ScientificPaperBuilder builder) {
         super(builder);
     }
 
     @Override
-    protected void checkFileExtension(File paper) throws NoMainDocumentFoundException, IOException {
-        if(!paper.getName().endsWith("zip")){
-            throw  new IOException();
+    protected File extract(File archive) throws IOException {
+        try {
+            ZipFile zipFile = new ZipFile(archive);
+            File extractedArchiveDir = initDestinationDir(archive);
+            zipFile.extractAll(extractedArchiveDir.getAbsolutePath());
+
+            return extractedArchiveDir;
+
+        } catch (ZipException e) {
+            LOG.fatal("Catch ZipException this message: " + e.getMessage() + System.lineSeparator() + "And throw IOException with the same message.");
+            throw new IOException(e.getMessage());
         }
     }
 
-    @Override
-    protected File extract(File archive) {
-        return null;
+    private File initDestinationDir(File rootFile) throws IOException {
+        File directory = new File(rootFile.getParentFile().getAbsolutePath() + FILE_SEPARATOR + DEST_DIR_NAME);
+        if(!directory.exists()) {
+            if(!directory.mkdir()) {
+                LOG.fatal("Throw IOException this message: Could not create directory: " + directory.getAbsolutePath());
+                throw new IOException("Could not create directory: " + directory.getAbsolutePath());
+            }
+        }
+        int versionNo = 0;
+        while(new File(directory.getAbsolutePath() + FILE_SEPARATOR + SUB_DIR_NAME + versionNo).exists() == true) {
+            versionNo++;
+        }
+        File destinationDir = new File(directory.getAbsolutePath() + FILE_SEPARATOR + SUB_DIR_NAME + versionNo);
+        if(!destinationDir.mkdir()) {
+            LOG.fatal("Throw IOException this message: Could not create directory: " + destinationDir.getAbsolutePath());
+            throw new IOException("Could not create directory: " + destinationDir.getAbsolutePath());
+        }
+
+        return destinationDir;
     }
 }
