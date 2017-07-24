a8a604f88264406f783d1103602f3723805f5ef0
ptamas <p.tamas93@gmail.com>
2017-07-24 13:10:12 +0200

    - add new method

--- spp-core/src/main/java/hu/uni/miskolc/iit/spp/core/model/SupportedFileNames.java
+++ spp-core/src/main/java/hu/uni/miskolc/iit/spp/core/model/SupportedFileNames.java
@@ -4,6 +4,7 @@ public enum SupportedFileNames {
 	MAIN("main"),
 	PAPER("paper");
 	
+	private static String[] stringValues;
 	private String stringValue;
 
 	private SupportedFileNames(String stringValue) {
@@ -12,5 +13,15 @@ public enum SupportedFileNames {
 
 	public String getStringValue() {
 		return stringValue;
+	}
+
+	public static String[] getStringValues() {
+		stringValues = new String[SupportedFileNames.values().length];
+		int i = 0;
+		for(SupportedFileNames name : SupportedFileNames.values()) {
+			stringValues[i] = name.getStringValue();
+			i++;
+		}
+		return stringValues;
 	}
 }
