c8e8104adf126edba4d600d3ce41dafed1fade2f
ptamas <p.tamas93@gmail.com>
2017-07-24 13:04:23 +0200

    - reorganize the methods

--- spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/Latex2PDFCompiler.java
+++ spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/Latex2PDFCompiler.java
@@ -1,22 +1,19 @@
 package hu.uni.miskolc.iit.spp.latex.compile;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-import hu.uni.miskolc.iit.spp.core.model.UsedDirectoryNames;
-import hu.uni.miskolc.iit.spp.core.model.exception.ConversionToPDFException;
+import hu.uni.miskolc.iit.spp.core.model.SupportedCompileableTextFileExtensions;
 import hu.uni.miskolc.iit.spp.core.model.exception.SearchedFileNotExistsException;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public abstract class Latex2PDFCompiler {
 
-	private static final String FILE_SEPARATOR = System.getProperty("file.separator");
-	private static final String DESTINATION_DIR_NAME = UsedDirectoryNames.DIR_FOR_PDF_FILE.getStringValue();
-	private static final String SOURCE_DIR_NAME = UsedDirectoryNames.DIR_FOR_EXTRACT_FILES.getStringValue();
-	private static final String SUBDIR_NAME = "version_"; 
 	private static Logger LOG = LogManager.getLogger(Latex2PDFCompiler.class);
-	private File sourceDir;
 	protected String compilerArg;
 	protected String outputDirArg;
 
@@ -24,59 +21,58 @@ public abstract class Latex2PDFCompiler {
 		init();
 	}
 	
-	public File generatePDFFile(File paper) throws ConversionToPDFException {
+	public File generatePDFFile(File texFile, File destinationDir) throws IOException {
 		try {
-			File destinationDir = initDestinationDir(paper);
-			this.sourceDir = findSourceDir(paper);
-			File pdf = compile(sourceDir, destinationDir);
+			File pdf = compile(texFile, destinationDir);
 			return pdf;
-		} catch (SearchedFileNotExistsException e) {
-			LOG.fatal("Catch SearchedFileNotExistsException this message: " + e.getMessage() + "\nAnd throw ConversionToPDFException with the same message.");
-			throw new ConversionToPDFException(e.getMessage());
-		} catch (IOException e) {
-			LOG.fatal("Catch IOException this message: " + e.getMessage() + "\nAnd throw ConversionToPDFException with the same message.");
-			throw new ConversionToPDFException(e.getMessage());
+
+		} catch(SearchedFileNotExistsException e) {
+			LOG.fatal("Catch SearchedFileNotExistsException this message: " + e.getMessage() + System.lineSeparator() + "And throw IOException with the same message.");
+			throw new IOException(e.getMessage());
 		}
+	}
+
+	protected File compile(File texFile, File destinationDir) throws SearchedFileNotExistsException, IOException {
+		Process process = Runtime.getRuntime().exec(command4Terminal(texFile, destinationDir));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+		String string;
+		while((string = reader.readLine()) != null) {
+		}
+
+		String fileName = FilenameUtils.getBaseName(texFile.getName());
+
+		return findPDFFile(destinationDir, fileName);
 	}
 
 	protected abstract void init();
-	
-	protected abstract File compile(File sourceDir, File destinationDir) throws SearchedFileNotExistsException, IOException;
-	
-	protected abstract String commandForTerminal(File sourceDir, File destinationDir) throws SearchedFileNotExistsException;
-	
-	private File initDestinationDir(File paper) throws IOException {
-		File directory = new File(paper.getParentFile().getAbsolutePath() + FILE_SEPARATOR + DESTINATION_DIR_NAME);
-		if(directory.exists() == false) {
-			if(directory.mkdir() == false) {
-				LOG.fatal("Throw IOException this message: Could not create directory: " + directory.getAbsolutePath());
-				throw new IOException("Could not create directory: " + directory.getAbsolutePath());
+	protected abstract String command4Terminal(File texFile, File destinationDir);
+
+	private File findPDFFile(File sourceDir, String fileName) throws SearchedFileNotExistsException {
+		File[] dirFiles = sourceDir.listFiles();
+		if(dirFiles != null && dirFiles.length > 0) {
+			for(File file : dirFiles) {
+				if(file.isDirectory()) {
+					return findPDFFile(file, fileName);
+				}
+				if(isSameName(file, fileName) && isTexFile(file)) {
+					return file;
+				}
 			}
 		}
-		int versionNo = 0;
-		while(new File(directory.getAbsolutePath() + FILE_SEPARATOR + SUBDIR_NAME + versionNo).exists() == true) {
-			versionNo++;
-		}
-		File destinationDir = new File(directory.getAbsolutePath() + FILE_SEPARATOR + SUBDIR_NAME + versionNo);
-		if(destinationDir.mkdir() == false) {
-			LOG.fatal("Throw IOException this message: Could not create directory: " + destinationDir.getAbsolutePath());
-			throw new IOException("Could not create directory: " + destinationDir.getAbsolutePath());
-		}
-		return destinationDir;
-	}
-	
-	private File findSourceDir(File paper) throws IOException {
-		File rootDir = new File(paper.getParentFile().getAbsolutePath() + FILE_SEPARATOR + SOURCE_DIR_NAME);
-		if(rootDir.exists() == false) {
-			LOG.fatal("Throw IOException this message: Directory with tex file not exists.");
-			throw new IOException("Directory with tex file not exists.");
-		}
-		int subdirsNo = rootDir.listFiles().length;
-		File actualSubdir = new File(rootDir.getAbsolutePath() + FILE_SEPARATOR + SUBDIR_NAME + (subdirsNo - 1));
-		return actualSubdir;
+		LOG.fatal("Throw SearchedFileNotExistsException this message: Could not find .tex file with this name: " + fileName);
+		throw new SearchedFileNotExistsException("Could not find .tex file with this name: " + fileName);
 	}
 
-	public File getSourceDir() {
-		return sourceDir;
+	private boolean isSameName(File file, String name) {
+		String fileName = FilenameUtils.getBaseName(file.getName());
+
+		return fileName.equals(name);
+	}
+
+	private boolean isTexFile(File file) {
+		String fileExtension = FilenameUtils.getExtension(file.getName());
+
+		return fileExtension.equals(SupportedCompileableTextFileExtensions.TEX.getStringValue());
 	}
 }
--- spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/WindowsLatexPDFCompiler.java
+++ spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/WindowsLatexPDFCompiler.java
@@ -1,22 +1,9 @@
 package hu.uni.miskolc.iit.spp.latex.compile;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import org.apache.commons.io.FilenameUtils;
-
-import hu.uni.miskolc.iit.spp.core.model.SupportedCompileableTextFileExtensions;
-import hu.uni.miskolc.iit.spp.core.model.SupportedFileNames;
-import hu.uni.miskolc.iit.spp.core.model.SupportedGeneratedFileExtensions;
-import hu.uni.miskolc.iit.spp.core.model.exception.SearchedFileNotExistsException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class WindowsLatexPDFCompiler extends Latex2PDFCompiler {
 
-	private static Logger LOG = LogManager.getLogger(WindowsLatexPDFCompiler.class);
 	private String includeDirArg;
 	
 	protected WindowsLatexPDFCompiler() {
@@ -31,90 +18,13 @@ public class WindowsLatexPDFCompiler extends Latex2PDFCompiler {
 	}
 
 	@Override
-	protected File compile(File sourceDir, File destinationDir) throws SearchedFileNotExistsException, IOException {
-		Process process = Runtime.getRuntime().exec(commandForTerminal(sourceDir, destinationDir));
-		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		
-		String string;
-		while((string = reader.readLine()) != null) {
-		}
-		
-		return findPDFFile(destinationDir);
-	}
-
-	@Override
-	protected String commandForTerminal(File sourceDir, File destinationDir) throws SearchedFileNotExistsException {
-		File mainFile = findTexFile(sourceDir);
-		String fullCommand = 	this.compilerArg + 
+	protected String command4Terminal(File texFile, File destinationDir) {
+		String fullCommand = 	this.compilerArg +
 								this.includeDirArg + 
-								sourceDir.getAbsolutePath() + 
+								texFile.getParentFile().getAbsolutePath() +
 								this.outputDirArg + 
 								destinationDir.getAbsolutePath() + " " + 
-								mainFile.getName();
+								texFile.getName();
 		return fullCommand;
 	}
-	
-	private File findTexFile(File sourceDir) throws SearchedFileNotExistsException {
-		try {
-			File texFile = findFile(sourceDir, SupportedCompileableTextFileExtensions.TEX.getStringValue());
-			return texFile;
-		} catch (SearchedFileNotExistsException e) {
-			LOG.fatal("Catch SearchedFileNotExistsException and throw a new with this message: Not found main.tex or paper.tex file.");
-			throw new SearchedFileNotExistsException("Not found main.tex or paper.tex file.");
-		}
-	}
-	
-	private File findPDFFile(File sourceDir) throws SearchedFileNotExistsException {
-		try {
-			File pdfFile = findFile(sourceDir, SupportedGeneratedFileExtensions.PDF.getStringValue());
-			return pdfFile;
-		} catch (SearchedFileNotExistsException e) {
-			LOG.fatal("Catch SearchedFileNotExistsException and throw a new with this message: Not found main.pdf or paper.pdf file.");
-			throw new SearchedFileNotExistsException("Not found main.pdf or paper.pdf file.");
-		}
-	}
-	
-	private File findFile(File sourceDir, String extension) throws SearchedFileNotExistsException {
-		File[] dirFiles = sourceDir.listFiles();
-		if(dirFiles != null && dirFiles.length > 0) {
-			for(File file : dirFiles) {
-				if(file.isDirectory()) {
-					return findFile(sourceDir, extension);
-				}
-				if(isAccaptedName(file) && (isTexFile(file) || isPDFFile(file))) {
-					if(isThatExtension(file, extension)) {
-						return file;
-					}
-				}
-			}
-		}
-		LOG.fatal("Throw SearchedFileNotExistsException without message.");
-		throw new SearchedFileNotExistsException();
-	}
-
-	private boolean isAccaptedName(File file) {
-		String fileName = FilenameUtils.getBaseName(file.getName()).toLowerCase();
-		for(SupportedFileNames name : SupportedFileNames.values()) {
-			if(fileName.equals(name.getStringValue())) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private boolean isTexFile(File file) {
-		String fileExtension = FilenameUtils.getExtension(file.getName());
-		return fileExtension.equals(SupportedCompileableTextFileExtensions.TEX.getStringValue());
-	}
-	
-	private boolean isPDFFile(File file) {
-		String fileExtension = FilenameUtils.getExtension(file.getName());
-		return fileExtension.equals(SupportedGeneratedFileExtensions.PDF.getStringValue());
-	}
-	
-	private boolean isThatExtension(File file, String extension) {
-		String fileExtension = FilenameUtils.getExtension(file.getName());
-		return fileExtension.equals(extension);
-	}
-
 }
