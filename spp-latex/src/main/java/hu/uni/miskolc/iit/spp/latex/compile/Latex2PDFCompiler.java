32b826d3128bbc5019eb74882a68fd8551d75af6
ptamas <p.tamas93@gmail.com>
2017-07-25 14:47:57 +0200

    - compile method implementation moved to generatedPDFFile method

--- spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/Latex2PDFCompiler.java
+++ spp-latex/src/main/java/hu/uni/miskolc/iit/spp/latex/compile/Latex2PDFCompiler.java
@@ -20,29 +20,24 @@ public abstract class Latex2PDFCompiler {
 	protected Latex2PDFCompiler() {
 		init();
 	}
-	
+
 	public File generatePDFFile(File texFile, File destinationDir) throws IOException {
 		try {
-			File pdf = compile(texFile, destinationDir);
-			return pdf;
+			Process process = Runtime.getRuntime().exec(command4Terminal(texFile, destinationDir));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			String string;
+			while((string = reader.readLine()) != null) {
+			}
+
+			String fileName = FilenameUtils.getBaseName(texFile.getName());
+
+			return findPDFFile(destinationDir, fileName);
 
 		} catch(SearchedFileNotExistsException e) {
 			LOG.fatal("Catch SearchedFileNotExistsException this message: " + e.getMessage() + System.lineSeparator() + "And throw IOException with the same message.");
 			throw new IOException(e.getMessage());
 		}
-	}
-
-	protected File compile(File texFile, File destinationDir) throws SearchedFileNotExistsException, IOException {
-		Process process = Runtime.getRuntime().exec(command4Terminal(texFile, destinationDir));
-		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-		String string;
-		while((string = reader.readLine()) != null) {
-		}
-
-		String fileName = FilenameUtils.getBaseName(texFile.getName());
-
-		return findPDFFile(destinationDir, fileName);
 	}
 
 	protected abstract void init();
