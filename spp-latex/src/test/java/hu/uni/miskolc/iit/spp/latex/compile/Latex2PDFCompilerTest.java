de3ba4b27b453dc423c4798affbddc384e3da8d4
ptamas <p.tamas93@gmail.com>
2017-07-28 14:32:28 +0200

    - add new static fields, removed unused fields, removed tearDown method, removed unused imports

--- spp-latex/src/test/java/hu/uni/miskolc/iit/spp/latex/compile/Latex2PDFCompilerTest.java
+++ spp-latex/src/test/java/hu/uni/miskolc/iit/spp/latex/compile/Latex2PDFCompilerTest.java
@@ -1,6 +1,5 @@
 package hu.uni.miskolc.iit.spp.latex.compile;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
@@ -8,36 +7,21 @@ import java.io.File;
 
 public abstract class Latex2PDFCompilerTest {
 
-    protected static final String FILE_SEPARATOR = System.getProperty("file.separator");
-    protected static final String RESOURCE_DIR = "src" + FILE_SEPARATOR + "resources" + FILE_SEPARATOR + "compilePackage";
-
-    protected static File texFile;
-    protected static File pdfFile;
-    protected static File directory;
+    protected static File testTexFile;
+    protected static File testDirectory;
 
     protected Latex2PDFCompiler compilerLinux;
     protected Latex2PDFCompiler compilerWindows;
 
     @BeforeClass
     public static void beforeClass() {
-        texFile = new File(RESOURCE_DIR + FILE_SEPARATOR + "main.tex");
-        pdfFile = new File(RESOURCE_DIR + FILE_SEPARATOR + "main.pdf");
-        directory = new File(RESOURCE_DIR);
+        testTexFile = new File("test.tex").getAbsoluteFile();
+        testDirectory = new File("directory");
     }
 
     @Before
     public void setUp() {
         compilerLinux = new LinuxLatexPDFCompiler();
         compilerWindows = new WindowsLatexPDFCompiler();
-    }
-
-    @After
-    public void tearDown() {
-        File[] dirFiles = directory.listFiles();
-        for(File file : dirFiles) {
-            if(!file.getName().equals(texFile.getName())) {
-                file.delete();
-            }
-        }
     }
 }
