450b6698f88b812484122bd15605169f162c0fb7
ptamas <p.tamas93@gmail.com>
2017-07-28 14:35:01 +0200

    - removed unused imports, removed generatePDFFileTest_XXX method, use new static fields

--- spp-latex/src/test/java/hu/uni/miskolc/iit/spp/latex/compile/LinuxLatexPDFCompilerTest.java
+++ spp-latex/src/test/java/hu/uni/miskolc/iit/spp/latex/compile/LinuxLatexPDFCompilerTest.java
@@ -2,23 +2,13 @@ package hu.uni.miskolc.iit.spp.latex.compile;
 
 import org.junit.*;
 
-import java.io.File;
-import java.io.IOException;
-
 import static org.junit.Assert.*;
-import static org.junit.Assume.assumeNoException;
-import static org.junit.Assume.assumeTrue;
 
 public class LinuxLatexPDFCompilerTest extends Latex2PDFCompilerTest {
 
     @Before
     public void setUp() {
         super.setUp();
-    }
-
-    @After
-    public void tearDown() {
-        super.tearDown();
     }
 
     @Test
@@ -31,8 +21,6 @@ public class LinuxLatexPDFCompilerTest extends Latex2PDFCompilerTest {
 
     @Test
     public void command4TerminalTest() {
-        File testTexFile = new File("test.tex");
-        File testDirectory = new File("directory");
         String command = compilerLinux.command4Terminal(testTexFile, testDirectory);
 
         boolean condition_1 = command.contains(LinuxArgs.COMPILER.getArgument());
@@ -41,19 +29,5 @@ public class LinuxLatexPDFCompilerTest extends Latex2PDFCompilerTest {
         boolean condition_4 = command.contains(testTexFile.getName());
 
         assertTrue(condition_1 && condition_2 && condition_3 && condition_4);
-    }
-
-    @Test
-    public void generatePDFFileTest_Linux() throws IOException {
-        assumeTrue("Ignore test, because can't run this operation system.", System.getProperty("os.name").toLowerCase().contains("linux"));
-        try {
-            Runtime.getRuntime().exec("pdflatex -version");
-        } catch(Exception e) {
-            assumeNoException("Ignore test, because can't compile .tex files.", e);
-        }
-
-        compilerLinux.generatePDFFile(texFile, directory);
-
-        assertTrue(pdfFile.exists());
     }
 }
--- spp-latex/src/test/java/hu/uni/miskolc/iit/spp/latex/compile/WindowsLatexPDFCompilerTest.java
+++ spp-latex/src/test/java/hu/uni/miskolc/iit/spp/latex/compile/WindowsLatexPDFCompilerTest.java
@@ -1,15 +1,9 @@
 package hu.uni.miskolc.iit.spp.latex.compile;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
-
 import static org.junit.Assert.*;
-import static org.junit.Assume.assumeNoException;
-import static org.junit.Assume.assumeTrue;
 
 public class WindowsLatexPDFCompilerTest extends Latex2PDFCompilerTest {
 
@@ -17,12 +11,6 @@ public class WindowsLatexPDFCompilerTest extends Latex2PDFCompilerTest {
     public void setUp() {
         super.setUp();
     }
-
-    @After
-    public void tearDown()  {
-        super.tearDown();
-    }
-
     @Test
     public void initTest() {
         boolean condition_1 = compilerWindows.compilerArg.equals(WindowsArgs.COMPILER.getArgument());
@@ -33,8 +21,6 @@ public class WindowsLatexPDFCompilerTest extends Latex2PDFCompilerTest {
 
     @Test
     public void command4TerminalTest() {
-        File testTexFile = new File("test.tex").getAbsoluteFile();
-        File testDirectory = new File("directory");
         String command = compilerWindows.command4Terminal(testTexFile, testDirectory);
 
         boolean condition_1 = command.contains(WindowsArgs.COMPILER.getArgument());
@@ -45,19 +31,5 @@ public class WindowsLatexPDFCompilerTest extends Latex2PDFCompilerTest {
         boolean condition_6 = command.contains(testTexFile.getName());
 
         assertTrue(condition_1 && condition_2 && condition_3 && condition_4 && condition_5 && condition_6);
-    }
-
-    @Test
-    public void generatePDFFileTest_Windows() throws IOException {
-        assumeTrue("Ignore test, because can't run this operation system.", System.getProperty("os.name").toLowerCase().contains("windows"));
-        try {
-            Runtime.getRuntime().exec("pdflatex -version");
-        } catch(Exception e) {
-            assumeNoException("Ignore test, because can't compile .tex files.", e);
-        }
-
-        compilerWindows.generatePDFFile(texFile, directory);
-
-        assertTrue(pdfFile.exists());
     }
 }
