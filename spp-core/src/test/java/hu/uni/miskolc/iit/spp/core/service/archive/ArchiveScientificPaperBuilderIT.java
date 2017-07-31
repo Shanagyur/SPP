64d0bff939426e3aae17b799a3d24e9c9fb19782
ptamas <p.tamas93@gmail.com>
2017-07-31 12:46:46 +0200

    - integration tests for archive package

---
+++ spp-core/src/test/java/hu/uni/miskolc/iit/spp/core/service/archive/ArchiveScientificPaperBuilderIT.java
@@ -0,0 +1,27 @@
+package hu.uni.miskolc.iit.spp.core.service.archive;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public abstract class ArchiveScientificPaperBuilderIT {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void build() throws Exception {
+    }
+
+    @Test
+    public void build1() throws Exception {
+    }
+
+}
