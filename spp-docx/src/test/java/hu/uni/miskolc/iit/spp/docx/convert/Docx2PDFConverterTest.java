1053719b0ec3b9a88814dee36595d860c3274eaa
PT <PT@192.168.2.13>
2018-03-06 13:53:07 +0100

    DocxConverterFactory and Docx2PDFConverter tests

--- spp-docx/src/test/java/hu/uni/miskolc/iit/spp/docx/convert/Docx2PDFConverterTest.java
+++ spp-docx/src/test/java/hu/uni/miskolc/iit/spp/docx/convert/Docx2PDFConverterTest.java
@@ -2,37 +2,68 @@ package hu.uni.miskolc.iit.spp.docx.convert;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.easymock.EasyMock;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import hu.uni.miskolc.iit.spp.core.model.exception.SearchedFileNotExistsException;
+
 public class Docx2PDFConverterTest {
+	
+	private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	private static final String SOURCE_DIR = "src" + FILE_SEPARATOR + "resources";
+	
+	private static File testFolder;
+	private static File docxFile;
+	
+	private Docx2PDFConverter mockConverter;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+		testFolder = new File(SOURCE_DIR + FILE_SEPARATOR + "directory");
+		testFolder.mkdir();
+		docxFile = new File(SOURCE_DIR + FILE_SEPARATOR + "goodFile.docx");
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		DocxConverterFactory mockFactory = EasyMock.mock(DocxConverterFactory.class);
+		EasyMock.expect(mockFactory.createDocxPdfConverter(docxFile)).andReturn(new Docx2PDFConverter("goodFile", new FileInputStream(docxFile)));
+		EasyMock.replay(mockFactory);
+		mockConverter = mockFactory.createDocxPdfConverter(docxFile);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if(testFolder.exists()) {
+			removeDirectory(testFolder);
+		}
 	}
+	
+	private void removeDirectory(File dir) {
+		 if(dir.isDirectory()) {
+			 File[] files = dir.listFiles();
+			 if(files != null && files.length > 0) {
+				 for(File aFile : files) {
+					 removeDirectory(aFile);
+				 }
+			 }
+			 dir.delete();
+		 } else {
+			 dir.delete();
+		 }
+	 }
 
 	@Test
-	public void testDocx2PDFConverter() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGeneratePDF() {
-		fail("Not yet implemented");
+	public void generatePDF() throws IOException, SearchedFileNotExistsException {
+		File pdf = mockConverter.generatePDF(testFolder);
+		
+		assertTrue(pdf.exists() && pdf.length() > 0);
 	}
 }
