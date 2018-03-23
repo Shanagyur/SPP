b17de8f9219bffe2c248ac504a36808fa0490617
PT <PT@192.168.2.13>
2018-03-23 14:55:45 +0100

    - remove unnecessary imports

--- spp-docx/src/main/java/hu/uni/miskolc/iit/spp/docx/ScientificPaperDocxBuilder.java
+++ spp-docx/src/main/java/hu/uni/miskolc/iit/spp/docx/ScientificPaperDocxBuilder.java
@@ -10,7 +10,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.io.FilenameUtils;
